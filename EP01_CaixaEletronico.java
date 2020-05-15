d1e5c937b702f74becca879ce5a4f9579f0411df
J Rafael R Nascimento <jose.rafael@usp.br>
2020-05-15 14:48:24 -0300

    Meu primeiro commit

---
+++ EP01_CaixaEletronico.java
@@ -0,0 +1,108 @@
+/*********************************************************************/
+/**   ACH2001 - Introdução à Programação                            **/
+/**   EACH-USP - Primeiro Semestre de 2020                          **/
+/**   <2020194> - <Norton Trevisan Roman>                           **/
+/**                                                                 **/
+/**   Primeiro Exercício-Programa                                   **/
+/**                                                                 **/
+/**   <Jose Rafael Rodrigues Nascimento>        <11847801>          **/
+/**                                                                 **/
+/**   <28/03/2020>                                                  **/
+/*********************************************************************/
+
+/*
+	Caixa eletrônico das Ilhas Weblands, com estoque ilimitado de cédulas
+	de B$50,00, B$10,00, B$5,00 e B$1,00.
+*/
+
+public class CaixaEletronico {
+	// Número de cédulas de B$50,00
+	static int n50;
+	
+	// Número de cédulas de B$10,00
+	static int n10;
+
+	// Número de cédulas de B$5,00
+	static int n5;
+	
+	// Número de cédulas de B$1,00
+	static int n1;
+
+	
+		/*
+		Determina a quantidade de cada nota necessária para totalizar
+		um determinado valor de retirada, de modo a minimizar a quantidade
+		de cédulas entregues.
+		
+		Abastece as variáveis globais n50,n10, n5 e n1 com seu respectivo
+		número de cédulas.
+		
+		Parâmetro:
+			valor - O valor a ser retirado
+		*/
+
+
+	static void fazRetirada(int valor) {
+	
+		//Recece o valor em B$ a ser retirado atraves do parâmetro "valor"
+		
+		
+		
+		//Caso valor negativo, atualiza as variaveis e sai do método
+		
+		if (valor < 0){
+			n50 = -1;
+			n10 = -1;
+			n5 = -1;
+			n1 = -1;
+			return;
+		}
+		
+		
+		
+		//Caso seja positivo, o método continuará e efetuará o cálculo das notas em valor decrescente
+		
+		if (valor >= 50){
+			n50 = valor/50;					//Faz a divisão inteira
+			valor = valor-(n50*50);			//Desconta o valor já calculado para o próximo IF avaliar
+		} else n50 = 0;						//Zera conforme solicitado
+										
+											//Mesma lógica para todos os IFs abaixo
+		
+			
+				if (valor >= 10){
+					n10 = valor/10;
+					valor = valor-(n10*10);
+				} else n10 = 0;
+					
+					
+							if (valor >= 5){
+								n5 = valor/5;
+								valor = valor-(n5*5);
+							} else n5 = 0;
+							
+								
+									if (valor >= 1){
+										n1 = valor;
+										valor = valor-n1;
+									} else n1 = 0;
+
+
+	}
+	
+
+	
+	
+	public static void main(String[] args) {
+	
+		fazRetirada(66);
+
+		System.out.println("Notas de 50: "+n50);
+		System.out.println("Notas de 10: "+n10);
+		System.out.println("Notas de 5:  "+n5);
+		System.out.println("Notas de 1:  "+n1);
+		System.out.println("\n\n");
+												
+
+	}
+}
---
+++ EP02_Raiz.java
@@ -0,0 +1,94 @@
+/*********************************************************************/
+/**   ACH2001 - Introdução à Programação                            **/
+/**   EACH-USP - Primeiro Semestre de 2020                          **/
+/**   <2020194> - <Norton Trevisan Roman>                           **/
+/**                                                                 **/
+/**   Segundo Exercício-Programa                                    **/
+/**                                                                 **/
+/**   <Jose Rafael Rodrigues Nascimento>        <11847801>          **/
+/**                                                                 **/
+/**   <23/04/2020>                                                  **/
+/*********************************************************************/
+
+public class Raiz {
+	/*
+		Calcula a raiz quadrada de um valor, com uma determinada
+		precisão. Retorna esse valor, ou -1 quando:
+		
+		* a < 0
+		* epsilon <= 0
+		* epsilon >= 1
+		
+		Parâmetro:
+			a - valor cuja raiz quadrada será calculada
+			epsilon - precisão do cálculo
+	*/
+	
+	
+	static double raizQuadrada(double a, double epsilon) {
+
+		
+		// Os valores recebidos pelos parâmetros são validados no início
+		if (a < 0 || epsilon <= 0 || epsilon >= 1)
+			return(-1);
+		
+		//Retorno padrão para raiz de 0
+		if (a == 0)
+			return (0);
+				
+				
+				//Método de Newton
+				
+				else {
+					
+					double x0, x1;		//variavel temporaria para calculo
+					
+					//Calculos iniciais 							
+					x0 = a/2;
+					x1 = (x0+(a/x0))/2;					
+
+					
+							//Laço de repetição enquanto o valor absoluto da diferença respeitar o método
+							while (absoluto(x1, x0) >= epsilon) {
+								
+								x0 = x1;
+								x1 = (x0+(a/x0))/2;	
+								
+							} 
+																
+							//Retorno do último valor após sair do loop, conforme definição do método
+							return(x1);
+				}
+
+				
+
+
+
+	}
+	
+	static double absoluto(double a, double b){
+		//Método para calcular o valor absoluto da diferença entre dois números
+		
+		if ((a-b) < 0)				//Se a diferença for negativa
+			return (-(a-b));		//Retornará a diferença com sinal invertido
+				else
+					return (a-b);	//Caso contrário, retorna a própria diferença
+		
+	}
+
+
+
+	public static void main(String[] args) {
+
+		// Exemplo de teste:
+		double valor[] = {-1, -1, -1, 0, 0, 0, 0, 1, 2, 3, 4, 9, 63, 81, 100};
+		double precisao[] = {0, 0.01, -1, 0.01, -1 , 0, 0.001, 0.001, 0.0001, 0.001,0.001, 0.001, 0.001, 0.9999, 1};
+		
+		for (int i=0; i<15; i++){
+				System.out.println("Raiz de: "+valor[i]+" com precisao de "+ precisao[i]+" = "+raizQuadrada(valor[i], precisao[i])+"\n\n");
+		
+		}
+		
+
+	}
+}
