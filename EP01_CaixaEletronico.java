4ef6558d066364c85b679dc44a5811036b32189b
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2020-05-15 14:57:20 -0300

    Update EP01_CaixaEletronico.java

--- EP01_CaixaEletronico.java
+++ EP01_CaixaEletronico.java
@@ -9,8 +9,21 @@
 /*********************************************************************/
 
 /*
+
+		Determina a quantidade de cada nota necessária para totalizar
+		um determinado valor de retirada, de modo a minimizar a quantidade
+		de cédulas entregues.
+		
+		Abastece as variáveis globais n50,n10, n5 e n1 com seu respectivo
+		número de cédulas.
+		
+		Parâmetro:
+			valor - O valor a ser retirado
+	
+
 	Caixa eletrônico das Ilhas Weblands, com estoque ilimitado de cédulas
 	de B$50,00, B$10,00, B$5,00 e B$1,00.
+
 */
 
 public class CaixaEletronico {
@@ -27,24 +40,11 @@ public class CaixaEletronico {
 	static int n1;
 
 	
-		/*
-		Determina a quantidade de cada nota necessária para totalizar
-		um determinado valor de retirada, de modo a minimizar a quantidade
-		de cédulas entregues.
-		
-		Abastece as variáveis globais n50,n10, n5 e n1 com seu respectivo
-		número de cédulas.
-		
-		Parâmetro:
-			valor - O valor a ser retirado
-		*/
-
+	
 
 	static void fazRetirada(int valor) {
 	
 		//Recece o valor em B$ a ser retirado atraves do parâmetro "valor"
-		
-		
 		
 		//Caso valor negativo, atualiza as variaveis e sai do método
 		
